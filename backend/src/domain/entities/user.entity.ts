dce5d16c7a83dfd67b2f84231106462f7d0b249c
Alan <alan.quintanapacheco@gmail.com>
2025-03-15 19:06:31 -0600

    🐛 REPAIRING: Fixed: Google Auth

--- backend/src/domain/entities/user.entity.ts
+++ backend/src/domain/entities/user.entity.ts
@@ -13,7 +13,17 @@ export interface User {
     comparePassword?(password: string): boolean
 }
 
-export interface UserEntity extends Omit<User, '_id' | 'comparePassword'> {
+export interface UserEntity {
     id?: string
+    name: string
+    email: string
+    password: string
+    preferredLanguage: 'es' | 'en'
+    theme: 'light' | 'dark'
+    defaultTimerPreset?: string
+    createdAt: Date
+    updatedAt: Date
+    googleId?: string
+    picture?: string
     comparePassword?(password: string): boolean
 }
