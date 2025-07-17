c6303bae863ee7ad937325a16d5a85dd72e56a6d
Alan Quintana <alan.quintanapacheco@gmail.com>
2025-07-16 19:48:58 -0600

    :bug: FIXED: TailwindCSS: migrating to v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'url'
 
 // https://vitejs.dev/config/
@@ -8,7 +7,7 @@ export default defineConfig(({ mode }) => {
 	const env = loadEnv(mode, process.cwd())
 
 	return {
-		plugins: [react(), tailwindcss()],
+		plugins: [react()],
 		resolve: {
 			alias: {
 				'@': fileURLToPath(new URL('./src', import.meta.url)),
