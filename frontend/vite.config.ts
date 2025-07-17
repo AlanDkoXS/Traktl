a81305adccbaaf61210cffadb45446b8b784b48f
Alan Quintana <alan.quintanapacheco@gmail.com>
2025-07-16 22:12:08 -0600

    :bug: UPDATED: TailwindCSS: v4

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -9,6 +10,7 @@ export default defineConfig(({ mode }) => {
 	return {
 		plugins: [
 			react(),
+			tailwindcss(),
 		],
 		resolve: {
 			alias: {
