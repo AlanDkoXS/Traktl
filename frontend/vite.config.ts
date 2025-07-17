26e0731611a776466c0334b917fec21b6f585016
Alan Quintana <alan.quintanapacheco@gmail.com>
2025-07-16 22:00:06 -0600

    :bug: FIXED: TailwindCSS v3 Rolling back again

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,6 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import { fileURLToPath, URL } from 'url'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -10,7 +9,6 @@ export default defineConfig(({ mode }) => {
 	return {
 		plugins: [
 			react(),
-			tailwindcss()
 		],
 		resolve: {
 			alias: {
